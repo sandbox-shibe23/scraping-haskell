6e7320df7894213606f6d209833ae8c0690affe9
shibe23 <shibe23.info@gmailcom>
2020-02-29 19:13:47 +0900

    memo: シンプルなHTTPリクエストの例

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.ByteString.Lazy.Char8 as L8
-import           Network.HTTP.Client
-import           Network.HTTP.Client.TLS
 import           Network.HTTP.Simple
 
 main :: IO ()
+main = do
+    response <- httpLBS "http://httpbin.org/get"
+    putStrLn $ "The status code was: " ++
+                show (getResponseStatusCode response)
+    print $ getResponseHeader "Content-Type" response
+    L8.putStrLn $ getResponseBody response
