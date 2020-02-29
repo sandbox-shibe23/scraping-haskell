9599bec28422dd703cd3a0a2137dd963e8e2cce6
shibe23 <shibe23.info@gmailcom>
2020-02-29 19:14:35 +0900

    memo: Managerを使ったHTTPリクエストの例
    - managerはTLSコネクションの使いまわしなど、接続に関する管理を行ってくれる
    - 基本はhttpLBSの引数を、managerを渡したserRequestManagerに置き換えるだけ
    - httpsサイトをリクエストすると、TlsNotSupportedになる

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.ByteString.Lazy.Char8 as L8
+import           Network.HTTP.Client    (defaultManagerSettings, newManager)
 import           Network.HTTP.Simple
 
 main :: IO ()
 main = do
-    response <- httpLBS "http://httpbin.org/get"
+    manager <- newManager defaultManagerSettings
+    
+    let request = setRequestManager manager "http://httpbin.org/get"
+    response <- httpLBS request
+
     putStrLn $ "The status code was: " ++
                 show (getResponseStatusCode response)
     print $ getResponseHeader "Content-Type" response
